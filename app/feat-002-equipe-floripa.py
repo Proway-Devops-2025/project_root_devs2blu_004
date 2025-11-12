bd66f6b6889b3b56b0f142c6ca46a506570c9403
Gabriel <ogabriel.msilva@gmail.com>
2025-11-12 20:41:22 -0300

    feat: modifica arquivo da equipe floripa

---
+++ app/feat-002-equipe-floripa.py
@@ -0,0 +1,30 @@
+add funcao giovanni(par1, par2);
+
+add fuction Predo
+def calculate_bill(pizza_size, toppings, drink=None):
+    prices = {
+        "small": 20,
+        "medium": 30,
+        "large": 40,
+        "topping": 3,
+        "drink": 8
+    }
+
+    total = prices[pizza_size] + prices["topping"] * len(toppings)
+    if drink:
+        total += prices["drink"]
+        print(f"🧾 Order: {pizza_size} pizza with {', '.join(toppings)} and a {drink}")
+    else:
+        print(f"🧾 Order: {pizza_size} pizza with {', '.join(toppings)}")
+
+    print(f"💰 Total: R${total},00")
+
+# Exemplo de uso
+calculate_bill("medium", ["bacon", "onion", "olives"], drink="guaraná")
+
+add function william(soma);
+
+float soma = num1 + num2;
+
+#Outra mudança
+add funcao GabrielM(par3, par4);
