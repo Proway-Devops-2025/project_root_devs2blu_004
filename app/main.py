b51492eb23dfc926bcaa0d7be6b7038a9a4c284b
Guilherme Cavaco <guihcavaco@gmail.com>
2025-11-12 19:25:47 -0300

    fear/002: nova feature no app

---
+++ app/main.py
@@ -0,0 +1,8 @@
+# Guilherme Cavaco - Nova feature
+for n in range(1, 11):
+    tipo = "par" if n % 2 == 0 else "ímpar"
+    potencia = n ** 2 if n % 2 == 0 else n ** 3
+    mensagem = f"{n} é {tipo} e a potencia é: {potencia}"
+    print(mensagem)
+
+#################################################################
